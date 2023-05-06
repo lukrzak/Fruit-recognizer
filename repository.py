b00d81346a997ed4b61c02744faf9f1eb714315e
lukrzak <lukrzak@wp.pl>
2023-05-06 16:14:52 +0200

    Created repository to get parsed data from direcotry

--- repository.py
+++ repository.py
@@ -1,25 +1,53 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
 import constraints as CONST
 
 
-def get_dataset(type: str):
-    path = CONST.TRAIN_DATA_PATH if type == "train" else CONST.TEST_DATA_PATH
-
+def get_dataset():
     data_generator = ImageDataGenerator(
-        rescale=1. / 255,
+        rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True
     )
 
-    dataset_generator = data_generator.flow_from_directory(
-        path,
+    train_dataset_generator = data_generator.flow_from_directory(
+        CONST.TRAIN_DATA_PATH,
         target_size=(CONST.TARGET_IMAGE_HEIGHT, CONST.TARGET_IMAGE_WIDTH),
+        class_mode='categorical',
         batch_size=CONST.BATCH_SIZE,
-        class_mode='binary',
         shuffle=True
     )
 
-    x_data, y_data = dataset_generator.next()
-    return x_data, y_data
+    test_dataset_generator = data_generator.flow_from_directory(
+        CONST.TEST_DATA_PATH,
+        target_size=(CONST.TARGET_IMAGE_HEIGHT, CONST.TARGET_IMAGE_WIDTH),
+        class_mode='categorical',
+        batch_size=CONST.BATCH_SIZE,
+        shuffle=True
+    )
+
+    print("Test data processing")
+    test_images, test_labels = [], []
+    for x_batch, y_batch in test_dataset_generator:
+        test_images.append(x_batch)
+        test_labels.append(y_batch)
+        if len(test_images) * test_dataset_generator.batch_size >= len(test_dataset_generator.filenames):
+            break
+    test_images = np.concatenate(test_images)
+    test_labels = np.concatenate(test_labels)
+
+    print("Train data processing")
+    train_images, train_labels = [], []
+    for x_batch, y_batch in train_dataset_generator:
+        train_images.append(x_batch)
+        train_labels.append(y_batch)
+        if len(train_images) * train_dataset_generator.batch_size >= len(train_dataset_generator.filenames):
+            break
+    train_images = np.concatenate(train_images)
+    train_labels = np.concatenate(train_labels)
+
+    print("Processing finished")
+
+    return train_images, train_labels, test_images, test_labels
